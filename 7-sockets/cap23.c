8d1d108e01b73969f31a7105de7d09b192ef64eb
FlickerStudio <flickerstudiord@gmail.com>
2016-05-31 10:32:48 -0400

    cap23 send request

--- 7-sockets/cap23.c
+++ 7-sockets/cap23.c
@@ -34,8 +34,19 @@ int main(int argc, char const *argv[])
     }
 
     connect( s, (struct sockaddr*)&mysocket, tam);
+    char request[] =
+        "GET / HTTP/1.1\r\n"
+        "Host: localhost:3000\r\n"
+        "User-Agent: Mozilla/5.0 (X11; Linux i686; rv:46.0) Gecko/20100101 Firefox/46.0\r\n"
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+        "Accept-Language: en-US,en;q=0.5\r\n"
+        "Accept-Encoding: gzip, deflate\r\n"
+        "Connection: keep-alive\r\n";
+    int numbytes = send(s, request, strlen(request), 0);
+    printf("bytes enviados: %d\n", numbytes);
+
     char buffer[KB];
-    int numbytes = recv(s, buffer, KB -1, 0);
+    numbytes = recv(s, buffer, KB -1, 0);
     printf("bytes recividos: %d\n", numbytes);
     printf("Recibido: %s\n", buffer);
 
